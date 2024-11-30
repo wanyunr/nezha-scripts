04f75e5cf9e34a0b9ebedd4206c7968d1ec37a24
UUBulb <35923940+uubulb@users.noreply.github.com>
2024-11-30 15:28:42 +0800

    uninstall first before installation (#13)

--- agent/install.sh
+++ agent/install.sh
@@ -154,7 +154,8 @@ install() {
 
     env="NZ_SERVER=$NZ_SERVER NZ_CLIENT_SECRET=$NZ_CLIENT_SECRET NZ_TLS=$NZ_TLS NZ_DISABLE_AUTO_UPDATE=$NZ_DISABLE_AUTO_UPDATE NZ_DISABLE_FORCE_UPDATE=$DISABLE_FORCE_UPDATE NZ_DISABLE_COMMAND_EXECUTE=$NZ_DISABLE_COMMAND_EXECUTE NZ_SKIP_CONNECTION_COUNT=$NZ_SKIP_CONNECTION_COUNT"
 
-    _cmd="sudo env $env $NZ_AGENT_PATH/nezha-agent service -c "$path" install"
+    sudo "${NZ_AGENT_PATH}"/nezha-agent service -c "$path" uninstall >/dev/null 2>&1
+    _cmd="sudo env $env $NZ_AGENT_PATH/nezha-agent service -c $path install"
     if ! eval "$_cmd"; then
         err "Install nezha-agent service failed"
         exit 1
